7ad0eecab9b52aa90591aa5c6fcebe0b9003a505
Tom Gieben <0085700@rocdeleijgraaf.nl>
2020-11-24 15:23:20 +0100

    groep selectie gemaakt php en css bestand

---
+++ php/groep.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Groep</title>
+    <meta charset=utf-8>
+    <meta name=description content="beschrijving">
+    <meta name=keywords content="trefwoord,trefwoord">
+    <link rel="stylesheet" href="https://github.com/Easytea/desplinter/tree/main/css/groep.css">
+
+</head>
+<body>
+
+<h1>In welke groep zit jij?</h1>
+
+    <form action="https://github.com/Easytea/desplinter/tree/main/php/forum.php" method="post" name="groep">
+        <input type= "radio" name ="1 t/m 4"> <label>Groep 1 t/m 4</label>
+        <br/>
+    	<input type= "radio" name ="5 t/m 8"> <label>Groep 5 t/m 8</label>
+
+
+    </form>
+    <br/>
+    <a href="https://github.com/Easytea/desplinter/tree/main/php/forum.php">
+    <input type="submit" name="volgende" value="volgende">
+
+
+</body>
+</html>
