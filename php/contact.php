0cee7be72e34d4c7710294c0de571696a6647f76
Tom Gieben <0085700@rocdeleijgraaf.nl>
2020-12-02 08:57:57 +0100

    mail systeem aangepast

--- php/contact.php
+++ php/contact.php
@@ -22,20 +22,23 @@
 </html>
 
 <?php
-if(isset($_POST['submit'])){
-    $to = "t.gieben@outlook.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    }
+if (empty($name) || empty($email) || empty($message))
+{
+    echo "Vul alle velden in.";
+}
+else
+{
+    mail("t.gieben@outlook.com", "De Splinter Bericht", $message , "Van: $name <
+        $email>");
+    echo "<script type='text/javascript'>alert('Je bericht is succesvol verzonden')
+     window.history.log(-1);
+    </script>";
+
+}
 ?>
+
 
