8396c02696fc4d7d6386edbf286999e21ffd856e
Tom Gieben <0085700@rocdeleijgraaf.nl>
2020-12-17 10:10:26 +0100

    ...

--- php/plus.php
+++ php/plus.php
@@ -1,4 +1,28 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Rekensite</title>
+    <link rel="stylesheet" href="http://localhost/desplinter/css/sommen.css">
+</head>
+<body>
+<div id="head">
+    <div class="header">
+        <img src="http://localhost/desplinter/image/logo.jpg" alt="logo" />
+    </div>
 
+    <div id="desplinter">
+        <h1>De Splinter</h1>
+    </div>
+</div>
+<ul>
+    <li><a href="http://localhost/desplinter/index.html">Home</a></li>
+    <li><a href="http://localhost/desplinter/informatie.html">Informatie</a></li>
+    <li><a href="http://localhost/desplinter/php/groep.php">Oefeningen</a></li>
+    <li><a href="http://localhost/desplinter/contact.html">Contact</a></li>
+    <li><?php echo "<li><a href=\"http://localhost/desplinter/php/plusmin.php?groep=$groep\"><img src=\"http://localhost/desplinter/image/pijl.png\" alt=\"pijl\" /></a></li>" ;
+        ?>
+</ul>
+<hr/>
 <!---->
 <?php
     echo"<h1>PLUS</h1>";
