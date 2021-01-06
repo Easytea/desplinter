984ee4ca96cd144bbdfcb4d361fec06a5fb6be20
Tom Gieben <0085700@rocdeleijgraaf.nl>
2021-01-06 14:14:49 +0100

    contact pagina bijgewerkt

---
+++ php/contactverwerk.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Groep</title>
+    <meta charset=utf-8>
+    <meta name=description content="beschrijving">
+    <meta name=keywords content="trefwoord,trefwoord">
+    <link rel="stylesheet" href="http://localhost/desplinter/css/contactverwerk.css">
+
+</head>
+<body>
+<div id="head">
+    <div class="header">
+        <img src="http://localhost/desplinter/image/logo.jpg" alt="logo" />
+    </div>
+
+    <div id="desplinter">
+        <h1>De Splinter</h1>
+    </div>
+</div>
+<ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="http://localhost/desplinter/informatie.html">Informatie</a></li>
+    <li><a href="http://localhost/desplinter/php/groep.php">Oefeningen</a></li>
+    <li><a href="http://localhost/desplinter/contact.html">Contact</a></li>
+</ul>
+<hr/>
+
+<!------------------------------------------------------------------------------------>
+
+<div id="container">
+    <div id="text">
+        <h1>Bedankt voor uw bericht!!</h1>
+    </div>
+            <div id="knop">
+                <a href="http://localhost/desplinter/index.html"></a>
+                <img src="http://localhost/desplinter/image/pijl.png" alt="pijl"/>
+            </div>
+    <div>
+        <img src="http://localhost/desplinter/image/bedankt.gif" alt="gif"/>
+    </div>
+
+    <div id="text2">
+        <h1>We proberen zo snel mogelijk te reageren.</h1>
+    </div>
+</div>
+
+
+
+
